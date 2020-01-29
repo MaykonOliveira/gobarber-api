49fe608dd36779e0de7077852b7ae559144244f3
MaykonOliveira <moliveira.anschau@gmail.com>
2020-01-28 22:34:24 -0300

    Create structure and the first migration

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Response, Request } from 'express'
-
-const routes = Router()
-
-routes.get('/', (req: Request, res: Response): Response => {
-  return res.json({ message: 'hello' })
-})
-
-export default routes
