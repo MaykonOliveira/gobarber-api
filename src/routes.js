b3dd96c1b5b1b53bbc8c06ab631b5ce7f0334670
MaykonOliveira <moliveira.anschau@gmail.com>
2020-01-28 22:53:49 -0300

    Connect to DB and create a User

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Maykon Anschau Oliveira',
+    email: 'moliveira.anschau@gmail.com',
+    password_hash: '21321321321',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
