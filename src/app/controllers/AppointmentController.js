b4a9e69185347368f3e6d63703a3ddcd017a4331
MaykonOliveira <moliveira.anschau@gmail.com>
2020-02-01 15:48:11 -0300

    Added offset paginatino to appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -8,9 +8,13 @@ import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { canceled_at: null },
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
