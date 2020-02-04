22fe2ed8ed63e2faa9d1c76d75780b8a82926fe7
MaykonOliveira <moliveira.anschau@gmail.com>
2020-02-03 23:49:41 -0300

    Add sentry

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+
+import 'express-async-errors';
 
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -10,11 +16,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(cors());
     this.server.use(
@@ -25,6 +35,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://e5db54de1d534fea9397585300446d1d@sentry.io/2225772',
+};
