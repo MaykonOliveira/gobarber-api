982241dc882d8d6be4b8ba2342db498a82411d2d
MaykonOliveira <moliveira.anschau@gmail.com>
2020-02-03 21:23:53 -0300

    Add mail send

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'a17f59ca80923b',
+    pass: 'bbbf29e51558ab',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
